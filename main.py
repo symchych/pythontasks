b8ae9e0f66dc49f6de45e937c251c3a41a3d5b43
v2meg <myroslavs@varyence.com>
2022-12-17 12:53:38 +0200

    фіва

--- main.py
+++ main.py
@@ -1,64 +1,196 @@
-# my_list = [1, 2, 2, 4, 4, 5, 6, 8, 10, 13, 22, 35, 52, 83]
+my_list = [1, 2, 2, 4, 4, 5, 6, 8, 10, 13, 22, 35, 52, 83]
 #
 # Write a program that prints out all the elements of the list that are higher than or equal 10.
-# Instead of printing the elements one by one, make a new list that has all the elements higher than or equal 10 from this list in it and print out this new list.
-# Ask the user for a number as input and return a list that contains only those elements from my_list that are higher than the number given by the user.
+# Instead of printing the elements one by one, make a new list that has all the elements higher
+# than or equal 10 from this list in it and print out this new list.
+# Ask the user for a number as input and return a list that contains only those elements
+# from my_list that are higher than the number given by the user.
+
+
+def filter_list_of_numbers(list_value):
+    user_number_typedin = input("number: \n")
+    user_number = int(user_number_typedin)
+    outcome = []
+    for l in list_value:
+        try:
+            if user_number >= l:
+                outcome.append(l)
+            else:
+                continue
+        except ValueError:
+            print("enter a correct value")
+    print(outcome)
+# filter_list_of_numbers(my_list)
+
 # EXERCISE 2: Working with Dictionaries
 # Using the following dictionary:
 
-def filter_list_of_numbers():
+employee = { "name": "tim", "age": 30, "birthday": "1990-03-10", "tim" : "DevOps Engineer" }
 
-    user_number = input("number: \n")
+# Write a Python Script that:
 
-    if user_number >= i:
-        return False;
-    else:
-        list+= user_number
-    print(1)
-
-print(f"20 days are {20*24*60} minutes")
-
-# employee = { “name”: “Tim”, “age”: 30, “birthday”: “1990-03-10”, “job”: “DevOps Engineer” }
-#
-# Write a Python Script that:DTT
-#
 # Updates the job to Software Engineer
 # Removes the age key from the dictionary
 # Loops through the dictionary and prints the key:value pairs one by one
+
+
+def edit_stuff(dict1):
+    dict1.update({"tim": "Software Engineer"})
+    dict1.pop("age")
+
+    thisdict = dict.keys(dict1)
+    for l in thisdict:
+        print(l+": "+dict1[l])
+#edit_stuff(employee)
+
+
 # Using the following 2 dictionaries:
-#
-# dict_one = {‘a’: 100, ‘b’: 400} dict_two = {‘x’: 300, ‘y’: 200}
-#
+
+dict_one = {"a": 100, "b": 400}
+dict_two = {"x": 300, "y": 200}
+
 # Write a Python Script that:
-#
+
 # Merges these two Python dictionaries into 1 new dictionary.
 # Sums up all the values in the new dictionary and print it out
 # Prints the max and minimum values of the dictionary
+
+def merge_and_sum(d1, d2):
+    d1.update(d2)
+    x=0
+    for l in d1.values():
+        try:
+            int(l)
+            x += l
+        except ValueError:
+            print("enter a correct value")
+    print(d1)
+    print(x)
+    highest = 0  # (so far)
+    for l in d1.values():
+        try:
+            int(l)
+            if l > highest:
+                highest = l
+            else:
+                continue
+        except ValueError:
+            print("enter a correct value")
+
+    print("the highest number is", highest)
+
+    lowest = highest
+    for l in d1.values():
+        try:
+            int(l)
+            if l < lowest:
+                lowest = l
+            else:
+                continue
+        except ValueError:
+            print("enter a correct value")
+
+    print("the lowest number is", lowest)
+#merge_and_sum(dict_one, dict_two)
+
 # EXERCISE 3: Working with List of Dictionaries
 # Using a list of 2 dictionaries:
 #
-# employees = [{ “name”: “Tina”, “age”: 30, “birthday”: “1990-03-10”, “job”: “DevOps Engineer”, “address”: { “city”: “New York”, “country”: “USA” } }, { “name”: “Tim”, “age”: 35, “birthday”: “1985-02-21”, “job”: “Developer”, “address”: { “city”: “Sydney”, “country”: “Australia” } }]
-#
+employees = [{ "name": "Tina", "age": 30, "birthday": "1990-03-10", "job": "DevOps Engineer", "address": { "city": "New York", "country": "USA" } }, { "name": "Tim", "age": 35, "birthday": "1985-02-21", "job": "Developer", "address": { "city": "Sydney", "country": "Australia" } }, { "name": "miro", "age": 22, "birthday": "1990-03-10", "job": "DevOps Engineer", "address": { "city": "New York", "country": "USA" } } ]
+# { "name": "Myroslav", "age": 22, "birthday": "2228-03-10", "job": "DevOps", "address": { "city": "Kyiv", "country": "EU" } }
 # Write a Python Program that:
 #
 # Prints out - the name, job and city of each employee using a loop. The program must work for any number of employees in the list, not just 2.
 # Prints the country of the second employee in the list by accessing it directly without the loop.
+
+def listofdictionary(list):
+    for i in list:
+        name = i.get("name")
+        job = i.get("job")
+        address = i.get("address")
+        city = address.get("city")
+        print(name+"'s job is", job, "and they live in", city)
+
+    print(list[1]["address"].get("country"))
+# listofdictionary(employees)
+
 # EXERCISE 4: Working with Functions
 # Write a function that accepts a list of dictionaries with employee age (see example list from the Exercise 3) and prints out the name and age of the youngest employee.
 # Write a function that accepts a string and calculates the number of upper case letters and lower case letters.
 # Write a function that prints the even numbers from a provided list.
-# For cleaner code, declare these functions in its own helper Module and use them in the main.py file
-# EXERCISE 5: Python Program 'Calculator’
+# For cleaner code, declare these functions in its own helper Module and use them in the main.py fil
+
+def getstufffromdictionary(list):
+    the_youngest_employee_list_id = 0
+    the_youngest_employee_age = list[0].get("age")
+    for i in list:
+        if int(i.get("age")) < the_youngest_employee_age:
+            the_youngest_employee_age = int(i.get("age"))
+            the_youngest_employee_list_id = list.index(i)
+        else:
+            continue
+    print(the_youngest_employee_age)
+    print(the_youngest_employee_list_id)
+    theYoungestEmployeeName = list[the_youngest_employee_list_id]["name"]
+    print("the name of the youngest employee is", theYoungestEmployeeName, "and he is", the_youngest_employee_age, "years old")
+# getstufffromdictionary(employees)
+
+# EXERCISE 5: Python Program 'Calculator"
 # Write a simple calculator program that:
 #
 # takes user input of 2 numbers and operation to execute
 # handles following operations: plus, minus, multiply, divide
 # does proper user validation and give feedback: only numbers allowed
-# Keeps the Calculator program running until the user types “exit”
+# Keeps the Calculator program running until the user types "exit"
 # Keeps track of how many calculations the user has taken, and when the user exits the calculator program, prints out the number of calculations the user did
 # Concepts covered: working with different data types, conditionals, type conversion, user input, user input validation
-#
-# EXERCISE 6: Python Program 'Guessing Game’
+def exitprogram(value):
+    if value == "exit":
+
+def calculator():
+    warning = "your number is of an incorrect format"
+    # if not isinstance(one, (int, float)):
+    #     print(warning)
+    # if not isinstance(two, (int, float)):
+    #     print(warning)
+    while True:
+        if action == "exit":
+
+        action = input("enter the action")
+        one = input("enter the first number")
+        while not one.isdigit():
+            print(warning)
+            one = input("enter the first number")
+
+
+        two = input("enter the second number")
+        while not two.isdigit():
+            print(warning)
+            two = input("enter the second number")
+
+
+        if action == "divide":
+            print (int(one) / int(two))
+        elif action == "plus":
+            print (int(one) + int(two))
+        elif action == "minus":
+            print (int(one) - int(two))
+        elif action == "multiply":
+            print (int(one) * int(two))
+
+        else:
+            "error"
+
+
+three = 3
+
+calculator()
+
+
+
+
+
+# EXERCISE 6: Python Program 'Guessing Game"
 # Write a program that:
 #
 # runs until the user guesses a number (hint: while loop)
@@ -120,21 +252,21 @@ print(f"20 days are {20*24*60} minutes")
 # Inheritance allows us to define a class that inherits all the methods and properties from another class.
 #
 # Create a Person class, which is the parent class of Student and Professor classes
-# This Person class has the following properties: “first_name”, “last_name” and “age”
-# and following method: “print_name”, which can print the full name
-# So you don’t need these properties and method in the other two classes. You can easily inherit these.
-# Change Student and Professor classes to inherit “first_name”, “last_name”, “age” properties and “print_name” method from the Person class
+# This Person class has the following properties: "first_name", "last_name" and "age"
+# and following method: "print_name", which can print the full name
+# So you don"t need these properties and method in the other two classes. You can easily inherit these.
+# Change Student and Professor classes to inherit "first_name", "last_name", "age" properties and "print_name" method from the Person class
 # EXERCISE 8: Working with Dates
 # Write a program that:
 #
-# accepts user’s birthday as input
+# accepts user"s birthday as input
 # and calculates how many days, hours and minutes are remaining till the birthday
 # prints out the result as a message to the user
 # EXERCISE 9: Working with Spreadsheets
 # Write a program that:
 #
-# reads the provided spreadsheet file “employees.xlsx” (see Download section at the bottom) with the following information/columns: “name”, “years of experience”, “job title”, “date of birth”
-# creates a new spreadsheet file “employees_sorted.xlsx” with following info/columns: “name”, “years of experience”, where the years of experience is sorted in descending order: so the employee name with the most experience in years is on top.
+# reads the provided spreadsheet file "employees.xlsx" (see Download section at the bottom) with the following information/columns: "name", "years of experience", "job title", "date of birth"
+# creates a new spreadsheet file "employees_sorted.xlsx" with following info/columns: "name", "years of experience", where the years of experience is sorted in descending order: so the employee name with the most experience in years is on top.
 # EXERCISE 10: Working with REST APIs
 # Write a program that:
 #
@@ -167,7 +299,7 @@ print(f"20 days are {20*24*60} minutes")
 # Print the name of each repository
 # Choose one specific repository and for that repository, list all the image tags inside, sorted by date. Where the most recent image tag is on top
 # EXERCISE 5: Python in Jenkins Pipeline
-# Create a Jenkins job that fetches all the available images from your application’s ECR repository using Python. It allows the user to select the image from the list through user input and deploys the selected image to the EC2 server using Python.
+# Create a Jenkins job that fetches all the available images from your application"s ECR repository using Python. It allows the user to select the image from the list through user input and deploys the selected image to the EC2 server using Python.
 #
 # Instructions:
 #
